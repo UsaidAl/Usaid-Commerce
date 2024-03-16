30b2d25ff12fa90b90dfbe9d370be0bd3cb5a383
UsaidAl <ali_usaid98@hotmail.com>
2024-03-16 18:56:19 +0000

    Search for one product by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,30 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  // This will get all the products
+  try {
+    const allProducts = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(allProducts);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+  // be sure to include its associated Category and Tag data
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// This will get one product by its 'id'
+router.get("/:id", async (async, res) => {
+  try {
+    const foundProduct = await Product.findOne({
+      include: [{ model: Category }, { model: Tag }],
+      where: { id: req.params.id },
+    });
+    res.status(200).json(foundProduct);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
